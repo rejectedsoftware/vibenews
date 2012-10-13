d4bdcb80969eee9a50558403a41a130e6fdd6108
Sönke Ludwig <sludwig@rejectedsoftware.com>
2012-10-13 17:22:27 +0200

    Added some basic media queries for narrow screens and a meta tag to improve mobile browser display.

--- source/vibenews/web.d
+++ source/vibenews/web.d
@@ -39,7 +39,7 @@ class WebInterface {
 
 		auto settings = new HttpServerSettings;
 		settings.port = 8009;
-		settings.bindAddresses = ["127.0.0.1"];
+		settings.bindAddresses = ["0.0.0.0"];
 
 		auto router = new UrlRouter;
 		router.get("/", &showIndex);
