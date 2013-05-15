f8cf4c14be2d4b84f0449a961bb59602329364aa
Sönke Ludwig <sludwig@rejectedsoftware.com>
2013-05-15 08:21:25 +0200

    Implement word based blacklisting.

--- source/vibenews/spamfilters/blacklist.d
+++ source/vibenews/spamfilters/blacklist.d
@@ -7,22 +7,28 @@
 */
 module vibenews.spamfilters.blacklist;
 
+import vibenews.message;
 import vibenews.vibenews;
 
+import std.array;
 import std.string;
+import std.uni;
 
 
 class BlackListSpamFilter : SpamFilter {
 	private {
 		string[] m_blockedIPs;
+		bool[string] m_blockedWords;
 	}
 
 	@property string id() { return "blacklist"; }
 
 	void setSettings(Json settings)
 	{
-		foreach( ip; settings.ips.opt!(Json[]) )
+		foreach (ip; settings.ips.opt!(Json[]))
 			m_blockedIPs ~= ip.get!string;
+		foreach (word; settings.words.opt!(Json[]))
+			m_blockedWords[word.get!string.toLower()] = true;
 	}
 
 	bool checkForBlock(ref const Article art)
@@ -31,6 +37,12 @@ class BlackListSpamFilter : SpamFilter {
 			foreach( prefix; m_blockedIPs )
 				if( ip.startsWith(prefix) )
 					return true;
+
+		if (art.getHeader("Subject").containsWords(m_blockedWords))
+			return true;
+		if (art.decodeMessage().containsWords(m_blockedWords))
+			return true;
+
 		return false;
 	}
 
@@ -38,4 +50,26 @@ class BlackListSpamFilter : SpamFilter {
 	{
 		return false;
 	}
+}
+
+
+private bool containsWords(string str, in bool[string] words)
+{
+	bool inword = false;
+	string wordstart;
+	while (!str.empty) {
+		auto ch = str.front;
+		auto isword = ch.isAlpha() || ch.isNumber();
+		if (inword && !isword) {
+			if (wordstart[0 .. wordstart.length - str.length].toLower() in words)
+				return true;
+			inword = false;
+		} else if (!inword && isword) {
+			wordstart = str;
+			inword = true;
+		}
+		str.popFront();
+	}
+
+	return true;
 }
