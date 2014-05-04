55d8376b08e44a7039452a0acbaf5031b7d138e9
Sönke Ludwig <sludwig@rejectedsoftware.com>
2014-05-04 12:36:04 +0200

    Add missing file.

---
+++ source/vibenews/webstrings.d
@@ -0,0 +1,10 @@
+/**
+	String definitions used in the web interface.
+
+	Copyright: © 2014 RejectedSoftware e.K.
+	License: Subject to the terms of the General Public License version 3, as written in the included LICENSE.txt file.
+	Authors: Sönke Ludwig
+*/
+module vibenews.webstrings;
+
+enum forumIndexNavCaption = "All forums";
