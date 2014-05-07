5b4f8d2bc05fad2f5caa4e5785e8a7d257332d04
Sönke Ludwig <sludwig@rejectedsoftware.com>
2014-05-07 10:53:19 +0200

    Avoid size jumping on Blink based Opera versions.

--- public/scripts/vibenews-reply.js
+++ public/scripts/vibenews-reply.js
@@ -4,15 +4,30 @@ function onTextChange()
 {
 	if (preview_timer) window.clearInterval(preview_timer);
 	preview_timer = window.setInterval(updatePreview, 1000);
+	adjustSizes();
+	return true;
 }
 
 function updatePreview()
 {
+	window.clearInterval(preview_timer);
+	preview_timer = null;
 	var message = $("#message");
 	var preview = $("#message-preview");
 	$.post("/markup", {message: message.val()}, function(data){
 		preview.html(data);
 		prettyPrint();
+		adjustSizes();
 		preview.show();
 	});
+	return false;
+}
+
+function adjustSizes()
+{
+	var message = $("#message");
+	var preview = $("#message-preview");
+	console.log($("#message-area").css("display"));
+	if ($("#message-area").css("display") == "flex")
+		message.height(preview.height());
 }
